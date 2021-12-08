211808f92a9a247ccb16e1d3cf956853e152c5f4
Jasper <jaspertruong@gmail.com>
2021-12-07 16:14:26 -0800

    started TransferController

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -0,0 +1,29 @@
+package com.techelevator.tenmo.controller;
+
+import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.dao.TransferDao;
+import com.techelevator.tenmo.model.Transfer;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transfers")
+@PreAuthorize("isAuthenticated()")
+public class TransferController {
+
+    private TransferDao transferDao;
+    public TransferController(TransferDao transferDao) {
+        this.transferDao = transferDao;
+    }
+
+    @GetMapping("/{id}")
+    public Transfer getTransferById(@PathVariable Long transferId) {
+        return transferDao.getTransfer(transferId);
+    }
+
+
+
+}
